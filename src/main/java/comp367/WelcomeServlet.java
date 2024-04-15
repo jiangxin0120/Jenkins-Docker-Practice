791827f85ca8cc6c35812b59b4a1ea7ad6e08d8d
jiangxin0120 <147292577+jiangxin0120@users.noreply.github.com>
2024-04-15 00:20:28 -0400

    Revert "Changes"

--- src/main/java/comp367/WelcomeServlet.java
+++ src/main/java/comp367/WelcomeServlet.java
@@ -3,28 +3,11 @@ package comp367;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.time.LocalTime;
 
 public class WelcomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html; charset=UTF-8");
-
-        // Get the current hour of the day
-        int hour = LocalTime.now().getHour();
-
-        // Determine the appropriate greeting based on the hour
-        String greeting;
-        if (hour < 12) {
-            greeting = "Good morning";
-        } else {
-            greeting = "Good afternoon";
-        }
-
-
-        String studentName = "Xin";
-
-        // Write the greeting to the response
-        resp.getWriter().write("<h2>" + greeting + ", " + studentName + ", Welcome to COMP367</h2>");
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h1>Welcome to COMP367</h1>");
     }
 }
